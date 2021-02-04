6962c00e80f805d827126c56a65fecb0705bbbe6
Shamnas <shamnastv@gmail.com>
2021-02-04 08:41:32 +0530

    update

--- ohsumed.sh
+++ ohsumed.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #SBATCH --job-name=ohsumed # Job name
 #SBATCH --ntasks=4 # Run on a single CPU
-#SBATCH --time=23:50:00 # Time limit hrs:min:sec
+#SBATCH --time=1:55:00 # Time limit hrs:min:sec
 #SBATCH --output=test_job%j%x.out # Standard output and error log
 #SBATCH --gres=gpu:1
 #SBATCH --partition=q1m_2h-1G
